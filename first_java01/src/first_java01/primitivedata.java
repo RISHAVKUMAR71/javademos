9985cc431442112d4c8030d46b47a1b1e2180115
unknown <rishavkmr21@gmail.com>
2022-07-14 09:30:08 +0530

    change

--- first_java01/src/first_java01/primitivedata.java
+++ first_java01/src/first_java01/primitivedata.java
@@ -3,20 +3,21 @@ package first_java01;
 public class primitivedata {
 
 	public static void main(String[] args) {
-        byte minByteValue = Byte.MIN_VALUE;
-        byte maxByteValue = Byte.MAX_VALUE;
-        System.out.println("Min byte value" + minByteValue);
-        System.out.println("Max byte value" + maxByteValue);
-        
-        int val=5;
-        float val2=5.23f;
-        double val3=5.23;
-        
-        val=6;
-        val2=6f;
-        val3=6d;
-        
-        System.out.println(val + val2 + val3);
+		byte minByteValue = Byte.MIN_VALUE;
+		byte maxByteValue = Byte.MAX_VALUE;
+		System.out.println("Min byte value" + minByteValue);
+		System.out.println("Max byte value" + maxByteValue);
+
+		int val = 5;
+		float val2 = 5.23f;
+		double val3 = 5.23;
+
+		val = 6;
+		val2 = 6f;
+		val3 = 6d;
+
+		System.out.println(val + val2 + val3);
+
 	}
 
 }
---
+++ first_java01/src/first_java01/stringmethod.java
@@ -0,0 +1,16 @@
+package first_java01;
+
+public class stringmethod {
+
+	public static void main(String[] args) {
+		String s = "Hello World";
+		System.out.println(s);
+		System.out.println("stringlength is " + s.length());
+		System.out.println("stringlength is " + s.indexOf('o'));
+		System.out.println("stringlength is " + s.charAt(7));
+		//System.out.println("stringlength is " + s.charAt(12));
+		System.out.println("command after exception");
+
+	}
+
+}
