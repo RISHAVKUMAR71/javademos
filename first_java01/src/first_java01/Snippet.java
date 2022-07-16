7046b8ea2b69580b3c154049651e0fbda4ab4184
unknown <rishavkmr21@gmail.com>
2022-07-16 13:33:32 +0530

    modified

--- first_java01/src/first_java01/Snippet.java
+++
@@ -1,6 +0,0 @@
-package first_java01;
-
-public class Snippet {
-	Webex Link: https://mphasis.webex.com/mphasis/j.php?MTID=m1df04e213f5333e8279ef11a19a4c72f | Password: Mphasis | Webinar No. 2510 545 7025
-}
-
