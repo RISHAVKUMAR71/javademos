fc9ea96044529b5b22fe5c8672fe9842f923e1b2
unknown <rishavkmr21@gmail.com>
2022-07-18 16:57:16 +0530

    modified

--- L1_proj/src/accessmodifier.java
+++ L1_proj/src/accessmodifier.java
@@ -1,8 +1,29 @@
+//
+//// default access modifier
+//class default_access{
+//void display() {
+//	System.out.println("default access modifer");
+//	
+//}
+//
+//}
+//
+//public class accessmodifier{
+//	public static void main(String[] args) {
+//		System.out.println("default access modifer");
+//		default_access def=new default_access();
+//		
+//		def.display();
+//		
+//	}
+//}
 
-// default access modifier
-class default_access{
-void display() {
-	System.out.println("default access modifer");
+
+
+//private access modifier
+class pri_access{
+private void display() {
+	System.out.println("using private access modifer");
 	
 }
 
@@ -10,10 +31,10 @@ void display() {
 
 public class accessmodifier{
 	public static void main(String[] args) {
-		System.out.println("default access modifer");
-		default_access def=new default_access();
+		System.out.println("private access modifer");
+		pri_access def=new pri_access();
 		
-		def.display();
+//	def.display();
 		
 	}
 }
