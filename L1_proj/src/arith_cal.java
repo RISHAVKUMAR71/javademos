b2baa9deec0abc99f6710a38e674d49bfd290ab9
unknown <rishavkmr21@gmail.com>
2022-07-19 14:54:38 +0530

    changed

--- L1_proj/src/arith_cal.java
+++ L1_proj/src/arith_cal.java
@@ -2,38 +2,45 @@ import java.util.Scanner;
 
 public class arith_cal {
 	public static void main(String[] args) {
+	
+		// data type deleared	
 		double first_input;
 		double second_input;
 		double result;
 		char operator;
 
 		System.out.print("Enter  any two number to perform operation = ");
+		// oblect created to take the input
 		Scanner scan = new Scanner(System.in);
-
-		first_input = scan.nextDouble();
-		second_input = (double) scan.nextFloat();
-
+		
+        //typecasting used
+		first_input = (float)scan.nextDouble();      //explicit typecast
+		second_input = (double) scan.nextFloat();    //implicit typecast
+		
 		System.out.print("choose any operator for operation (+, -, *, /)= ");
-
+		
+		// oblect created to take the input
 		operator = scan.next().charAt(0);
 
 		switch (operator) {
 		case '+':
-			result = first_input + second_input;
+			result = (first_input + second_input);
 			break;
 		case '-':
-			result = first_input - second_input;
+			result = (first_input - second_input);
 			break;
 		case '*':
-			result = first_input * second_input;
+			result = (first_input * second_input);
 			break;
 		case '/':
-			result = first_input / second_input;
+			result = (first_input / second_input);
 			break;
 		default:
 			System.out.printf("incorrect operator chosen");
 			return;
 		}
+		
+		//Result is displayed
 		System.out.print("Result:" + first_input + " " + operator + " " + second_input + " = " + result);
 
 	}
